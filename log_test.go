b35f9fc837d4e224f5d86d086831c3fb28cc57c1
Ravi Chamarthy <rchamarthy@users.noreply.github.com>
2024-07-19 15:13:35 -0700

    test: add tests for log
    Signed-off-by: Ravi Chamarthy <ravi@chamarthy.dev>

---
+++ log_test.go
@@ -0,0 +1,37 @@
+package chata_test
+
+import (
+	"context"
+	"log/slog"
+	"testing"
+
+	"github.com/rchamarthy/chata"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLog(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	assert := assert.New(t)
+
+	log := chata.Log(ctx)
+	assert.NotNil(log)
+	assert.Equal(log, chata.NilLogger())
+
+	ctx = context.WithValue(ctx, chata.LogKey, slog.Default())
+	log = chata.Log(ctx)
+	assert.NotNil(log)
+	assert.NotEqual(log, chata.NilLogger())
+	assert.Equal(log, slog.Default())
+
+	ctx = context.WithValue(ctx, chata.LogKey, "hello")
+	assert.Equal(chata.NilLogger(), chata.Log(ctx))
+}
+
+func TestNilLogger(t *testing.T) {
+	t.Parallel()
+
+	nilLogger := chata.NilLogger()
+	assert.NotNil(t, nilLogger)
+}
