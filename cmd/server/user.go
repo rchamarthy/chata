add8af7bbed80f6a19ea8f6b92771142b01b1d47
AntonK12733 <antonkingsley11@gmail.com>
2024-07-25 15:06:57 -0700

    Update user.go
    Please help with this, can't figure out why I'm getting errors

--- cmd/server/user.go
+++ cmd/server/user.go
@@ -36,21 +36,65 @@ func NewUserHandler(e *gin.Engine, config *Config) *UserHandler {
 }
 
 func (h *UserHandler) RegisterUser(c *gin.Context) {
-
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(400, gin.H{"error": "id is required"})
+		return
+	}
+	if e := h.db.RegisterUser(id); e != nil {
+		c.JSON(500, gin.H{"error": e.Error()})
+		return
+	}
 }
 
 func (h *UserHandler) DeleteUser(c *gin.Context) {
-
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(400, gin.H{"error": "id is required"})
+		return
+	}
+	if e := h.db.DeleteUser(id); e != nil {
+		c.JSON(500, gin.H{"error": e.Error()})
+		return
+	}
 }
 
 func (h *UserHandler) GetAllUsers(c *gin.Context) {
-
+	users, err := h.db.GetAllUsers()
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, users)
 }
 
 func (h *UserHandler) GetUser(c *gin.Context) {
-
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(400, gin.H{"error": "id is required"})
+		return
+	}
+	user, err := h.db.GetUser(id)
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, user)
 }
 
 func (h *UserHandler) UpdateUser(c *gin.Context) {
-
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(400, gin.H{"error": "id is required"})
+		return
+	}
+	var user User
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	if e := h.db.UpdateUser(id, user); e != nil {
+		c.JSON(500, gin.H{"error": e.Error()})
+		return
+	}
 }
